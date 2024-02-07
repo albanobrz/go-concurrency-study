fab759bc9b7d5909692c13de05f35235f301b7a2
Albano Braz <albano.braz@outlook.com>
2024-02-07 11:11:07 -0300

    first commit

---
+++ first-exemple/go.mod
@@ -0,0 +1,3 @@
+module first-exemple
+
+go 1.21.5
---
+++ first-exemple/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(s)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	words := []string{
+		"alpha",
+		"beta",
+		"delta",
+		"pi",
+		"eta",
+	}
+
+	wg.Add(len(words))
+
+	for i, x := range words {
+		go printSomething(fmt.Sprintf("%d: %s", i, x), &wg)
+	}
+
+	wg.Wait()
+
+	wg.Add(1)
+	printSomething("This is the second thing to be printed", &wg)
+}
+
+// key things:
+// the order of the outpust is random
+// the wait group can never be zero
