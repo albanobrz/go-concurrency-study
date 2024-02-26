20a2368a6d5280c8c3f68cd143a8bca8c05cb2ce
Albano Braz <albano.braz@outlook.com>
2024-02-26 19:07:28 -0300

    channels basics

---
+++ channel-select/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func server1(ch chan string) {
+	for {
+		time.Sleep(6 * time.Second)
+		ch <- "this is from server 1"
+	}
+}
+
+func server2(ch chan string) {
+	for {
+		time.Sleep(3 * time.Second)
+		ch <- "this is from server 2"
+	}
+}
+
+func main() {
+	fmt.Println("Select with channels")
+	fmt.Println("--------------------")
+
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	go server1(channel1)
+	go server2(channel2)
+
+	for {
+		select {
+		case s1 := <-channel1:
+			fmt.Println("Case 1", s1)
+
+		case s2 := <-channel1:
+			fmt.Println("Case 2", s2)
+
+		case s3 := <-channel2:
+			fmt.Println("Case 3", s3)
+
+		case s4 := <-channel2:
+			fmt.Println("Case 4", s4)
+
+			// default:
+			// avoiding deadlock
+		}
+	}
+}
+
+// when there is more than 1 case that receive from a channel, it choses a random
